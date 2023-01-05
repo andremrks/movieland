3279d13326ca6d9b4325210f0bc974df4689631e
André Marques <andrelcm89pt@gmail.com>
2023-01-05 20:28:27 +0000

    css changing

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import SearchIcon from './assets/search.svg'
 const API_URL = 'https://www.omdbapi.com?apikey=b96dd3e8'
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('')
-  const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('') // To allow searching...useState starts empty because in that case, the search starts empty
+  const [movies, setMovies] = useState([]) // allow to access our movies and setMovies function, an empty array 
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
     const data = await response.json()
 
-    setMovies(data.Search)
+    setMovies(data.Search) // The "Search" inside json
   }
 
   useEffect(() => {
@@ -27,14 +27,14 @@ const App = () => {
       
       <div className='search'>
         <input 
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm} // to be dinamic, now I can set the valeu for the input as my "searchTerm "
+          onChange={(e) => setSearchTerm(e.target.value)} // event to allow, on changing, "setSearchTerm" will be my new value
           placeholder="Search for movies"
         />
         <img 
           src={SearchIcon}
           alt="search"
-          onClick={() => searchMovies(searchTerm)}
+          onClick={() => searchMovies(searchTerm)} // onclick my new movie to search will be searchTerm which was what I type to search, in fact
         />
       </div>
 
@@ -42,8 +42,8 @@ const App = () => {
         movies?.length > 0
         ? (
           <div className='container'>
-            {movies.map((movie) => (
-              <MovieCard movie={movie} />
+            {movies.map((movie) => ( //looping for "movies" array, which is fetch for an APi, takin each individual "movie" then
+              <MovieCard movie={movie} /> //dinamicly passing as a "prop(movie={movie})" to component "MovieCard"
             ))}
           </div>
         ) :
